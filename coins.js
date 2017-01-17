d634f94a9530cfde9557a404aabaa7cf14da2e27
Harper Frankstone <Harper Frankstone>
2017-01-17 15:36:06 -0600

    more updates

--- coins.js
+++ coins.js
@@ -14,10 +14,13 @@ function coinCounter () {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
 
-  coinPurse.quarters = 0;
+  coinPurse.quarters = .25;
+  coinPurse.dimes = .1;
+  coinPurse.nickels = .05;
+  coinPurse.pennies = .01; 
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+var coins = coinCounter();
+console.log(coins);
