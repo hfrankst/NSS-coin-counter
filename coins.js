337da920b86d6e0bb9c5b95a456e5c37d86d8190
Harper Frankstone <Harper Frankstone>
2017-01-19 14:57:12 -0600

    actually finished this time

--- coins.js
+++ coins.js
@@ -1,60 +1,52 @@
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
-  }
- 
-*/
 
-var i = 0.94;
-  //the dollar amount will be input here
-function coinCounter () {
+var counter = document.getElementById("counter");
+var question = prompt("How much money do you have?");
 
-  var money = i * 100;
-
- // Initialize a JavaScript object to hold the coins
-  var coinPurse = {
+var coinPurse = {
     quarters: 0,
     dimes: 0,
     nickels: 0,
     pennies: 0,
   };
+  //the dollar amount will be input here
+function coinCounter () {
+
+  question *= 100;
+
+ // Initialize a JavaScript object to hold the coins
+  
 
   //using different variables to perfom the math functions and break down the dollar amount into coin count
 
   // finding the quarters
-    quartersRemainder = money % 25; 
+    quartersRemainder = question % 25; 
       // console.log(quartersRemainder); 
-    quarters = (money - quartersRemainder) / 25; 
-      console.log(quarters); 
+    coinPurse.quarters = (question - quartersRemainder) / 25; 
+      console.log(coinPurse.quarters); 
     
   //finding the dimes
     dimesRemainder = quartersRemainder % 10;
       // console.log(dimesRemainder); 
-    dimes = (quartersRemainder - dimesRemainder) / 10;
-      console.log(dimes); 
+    coinPurse.dimes = (quartersRemainder - dimesRemainder) / 10;
+      console.log(coinPurse.dimes); 
 
   //finding the nickels 
     nickelsRemainder = dimesRemainder % 5;
       // console.log(nickelsRemainder); 
-    nickels = (dimesRemainder - nickelsRemainder) / 5;
-      console.log(nickels); 
+    coinPurse.nickels = (dimesRemainder - nickelsRemainder) / 5;
+      console.log(coinPurse.nickels); 
 
   //finding the pennies
     penniesRemainder = nickelsRemainder % 1;
       // console.log(penniesRemainder);
-    pennies = (nickelsRemainder - penniesRemainder) / 1;
-      console.log(pennies);
+    coinPurse.pennies = (nickelsRemainder - penniesRemainder) / 1;
+      console.log(coinPurse.pennies);
 
 
    
  }
 
+coinCounter(); 
 
+counter.innerHTML = ("Your quarters are " + coinPurse.quarters + ", and your dimes are " + coinPurse.dimes + ", your nickels are " + coinPurse.nickels + ", your pennies are " + coinPurse.pennies + ".");
 
-var coins = coinCounter();
-console.log(coins); 
